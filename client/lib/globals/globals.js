a5b1a26cd7c654e4c77585434d41c92d36dd1086
Christoph Mussenbrock <christoph@etherisc.com>
2021-05-21 20:41:15 +0200

    meteor-kitchen 202105211841159004

--- client/lib/globals/globals.js
+++ client/lib/globals/globals.js
@@ -105,8 +105,8 @@ GoogleMaps.load({ v: '3', key: 'AIzaSyAuGOT9cn2Rp8O4gicXER-clMjVBMszlyY', librar
 
 round = (num, places) => Math.round(Math.sign(num) * (Math.abs(num) + Number.EPSILON) * 10**places) / 10**places;
 
-latLng2Pixel = function ({ lat, lng }) => ( ( round( lng, 1 ) + 40.1 ) * 1000 + round( lat, 1 ) + 20.1 ) * 10;
-pixel2LatLng = function ( pixel ) => {
+latLng2Pixel = ({ lat, lng }) => ( ( round( lng, 1 ) + 40.1 ) * 1000 + round( lat, 1 ) + 20.1 ) * 10;
+pixel2LatLng = ( pixel ) => {
      lat: ( ( pixel - trunc( pixel / 1000 ) * 1000 ) / 10 ) - 20.1,
      lng: ( trunc( pixel / 1000 ) / 10 ) - 40.1
 }
