4c02a29ee8ec8885896cc8f3508f6083a4996d89
Christoph Mussenbrock <christoph@etherisc.com>
2021-05-22 07:57:10 +0200

    meteor-kitchen 202105220557100347

--- imports/server/methods/count.js
+++ imports/server/methods/count.js
@@ -4,12 +4,16 @@ const countActivations = () => {
 	activations = Activations.find({});
 
 	activations.forEach((activation) => {
-		const count = RecordCounts.findOne({pixel: activation.pixel});
+		
+		const newPixel = latLng2PixelStr(activation.latitude, activation.longitude);
+		Activations.update({_id: activation._id}, {$set: {pixel: newPixel}});
+		
+		const count = RecordCounts.findOne({pixel: newPixel});
 		console.log(count);
 		if (count) {
-			RecordCounts.update({pixel: activation.pixel}, {$set: {count: count.count + 1}});
+			RecordCounts.update({pixel: newPixel}, {$set: {count: count.count + 1}});
 		} else {
-			RecordCounts.insert({pixel: activation.pixel, count: 1});
+			RecordCounts.insert({pixel: newPixel, count: 1});
 		}
 	});
 	
