b6c084d540e34fd5cb66f8caa38819f0021f9234
Christoph Mussenbrock <christoph@etherisc.com>
2021-06-01 15:15:54 +0200

    meteor-kitchen 202106011315546875

---
+++ imports/server/methods/activations-export.js
@@ -0,0 +1,31 @@
+/* Activations Reader */
+
+import { getMinioObject, putMinioObject } from '/imports/server/methods/minio.js';
+
+
+const activations_export = Meteor.wrapAsync(function (bucket, filename, filter, cb) {
+
+	const selected = Activations.find(filter).fetch();
+	
+	const activations_export = [];
+	
+	selected.forEach(item => activations_export.push(item));
+
+	const content = JSON.stringify(activations_export, null, 2);
+	const count = activations_export.length;
+	
+	
+	minioClient.putObject(bucket, filename, content, function(error, etag) {
+		if(error) {
+			error(`Error: ${error.message}`, {message: error.message, stack: error.stack});
+		  	cb(error, null); 
+		}
+		cb(null, count);
+	});	
+	
+}
+
+
+module.exports = { activations_export };
+
+
