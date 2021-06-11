33582e14b28d746890b23cd82055909d1b176ad8
Christoph Mussenbrock <christoph@etherisc.com>
2021-06-11 11:24:14 +0200

    meteor-kitchen 202106110924139980

--- imports/server/methods/bonga-gateway.js
+++ imports/server/methods/bonga-gateway.js
@@ -7,26 +7,45 @@ const key = 'm3fFvqHakNppHOw';
 const secret = 'yjokfBb6IQvAmVkiRAvpp9050gaZMD';
 const serviceID = '4849';
 
-const bongaApi = Meteor.wrapAsync(function ({endpoint = 'send-sms-v1', args = {}}, cb) {
+const bongaApi = Meteor.wrapAsync(function ({method='get', endpoint = 'send-sms-v1', args = {}}, cb) {
 	
 	info('bonga request', {endpoint, args});
 	
-	data = new FormData();
-	data.append('apiClientID', apiClientId);
-	data.append('key', key);
-	data.append('secret', secret);
+	let config;
 	
-	Object.keys(args).forEach(item => {
-		data.append(item, args[item]);
-	});
+	if (method === 'get') {
+		
+		config = {
+			method,
+			endpoint,
+			url,
+			params: args
+		};
+		
+	} else if method === 'post' {
+		
+		data = new FormData();
+		data.append('apiClientID', apiClientId);
+		data.append('key', key);
+		data.append('secret', secret);
+
+		Object.keys(args).forEach(item => {
+			data.append(item, args[item]);
+		});
+		
+		config = {
+			method,
+			endpoint,
+			url,
+			data,
+			headers: { "Content-Type": "multipart/form-data" }
+		};
+			
+	} else {
+		error(`bongaApi: Invalid Method ${method}`, {endpoint, args});
+	};
 	
-	
-	axios({
-    	endpoint,
-		url,
-    	data,
-		headers: { "Content-Type": "multipart/form-data" }
-	})
+	axios(config)
 	.then(function (response) {
 		const { status, statusText, headers, data } = response;
 		info('bongaApi response', {status, statusText, headers, data});
@@ -37,7 +56,13 @@ const bongaApi = Meteor.wrapAsync(function ({endpoint = 'send-sms-v1', args = {}
 		if (err.response) {
 			// The request was made and the server responded with a status code
 			// that falls out of the range of 2xx
-			message = `bongaApi received status ${err.response.status}`;
+			if (err.response.status === 666) { // we handle this as valid response
+				const { status, statusText, headers, data } = response;
+				info('bongaApi response', {status, statusText, headers, data});
+				cb(null, {status, statusText, headers, data});
+			} else {
+				message = `bongaApi received status ${err.response.status}`;
+			}
 		} else if (err.request) {
 			// The request was made but no response was received
 			// `error.request` is an instance of XMLHttpRequest in the browser and an instance of
@@ -53,13 +78,68 @@ const bongaApi = Meteor.wrapAsync(function ({endpoint = 'send-sms-v1', args = {}
 
 });
 
+const bongaFetchDeliveryReport = ({_id, unique_id}) => {
+	return bongaApi({
+		method: 'get',
+		endpoint: 'fetch-delivery',
+		unique_id
+	});
+};
 
 const bongaSMS = ({mobile_num, message}) => {
-	bongaApi({endpoint: 'send-sms-v1', args: {MSISDN: mobile_num, txtMessage: message, serviceID}});
+	const _id = Sms.insert({mobile_num, message, status: 0, status_message: 'sending...'});
+	try {
+		const {status, statusText, headers, data} = bongaApi({
+			method: 'post', 
+			endpoint: 'send-sms-v1', 
+			args: {
+				MSISDN: mobile_num, 
+				txtMessage: message, 
+				serviceID
+			}
+		});
+		if (status === 222) {
+			Sms.upsert({_id}, {$set: {
+				status, 
+				status_message: data.status_message, 
+				unique_id: data.unique_id, 
+				timestamp: Date.now(), 
+				credits: data.credits
+			}});
+			const {status, statusText, headers, data} = bongaFetchDeliveryReport({_id, data.unique_id});
+			if (status === 222) {
+				Sms.upsert({_id}, {$set: {
+					delivery_status_desc: data.delivery_status_desc,
+					date_received: data.date_received
+				}});
+			} else {
+				error(`Error fetching Delivery Report, unique_id=${data.unique_id}`, {status, statusText, headers, data});
+			}
+			// even if we cannot fetch Deliver Report, we consider the SMS as delivered
+			return 'SMS successfully sent';
+
+		} else if (status === 666) {
+			Sms.upsert({_id}, {$set: { status, status_message: data.status_message }});
+		} else {
+			Sms.upsert({_id}, {$set: { status: 999, 'Unknown Gateway Error' }});
+		};
+	} catch (error) {
+		Sms.upsert({_id}, {$set: { status: 999, status_message: 'Unknown Gateway Error'}});
+	}
+	return 'Error sending SMS';
 }
 
 const bongaSendMoney = ({mobile, message, amount}) => {
-	bongaApi({endpoint: 'b2c-send-money', args: {mobile: mobile_num, message, amount, smsServiceID: serviceID}});
+	const response = bongaApi({
+		method: 'post', 
+		endpoint: 'b2c-send-money', 
+		args: {
+			mobile: mobile_num, 
+			message, 
+			amount, 
+			smsServiceID: serviceID
+		}
+	});
 })	
 
 
--- server/publish/policies.js
+++ server/publish/policies.js
@@ -14,23 +14,6 @@ Meteor.publish("partner_policies_list", function(mobile_num) {
 	return Policies.find({mobile_num:mobile_num}, {});
 });
 
-Meteor.publish("partner_policies_list_paged", function(mobile_num, extraOptions) {
-	extraOptions.doSkip = true;
-	return Policies.find(databaseUtils.extendFilter({mobile_num:mobile_num}, extraOptions), databaseUtils.extendOptions({}, extraOptions));
-});
-
-Meteor.publish("partner_policies_list_paged_count", function(mobile_num, extraOptions) {
-	Counts.publish(this, "partner_policies_list_paged_count", Policies.find(databaseUtils.extendFilter({mobile_num:mobile_num}, extraOptions), { fields: { _id: 1 } }));
-});
-
-Meteor.methods({
-	"partnerPoliciesListPagedExport": function(mobile_num, extraOptions, exportFields, fileType) {
-		extraOptions.noPaging = true;
-		var data = Policies.find(databaseUtils.extendFilter({mobile_num:mobile_num}, extraOptions), databaseUtils.extendOptions({}, extraOptions)).fetch();
-		return objectUtils.exportArrayOfObjects(data, exportFields, fileType);
-	}
-});
-
 Meteor.publish("policy_list_paged", function(extraOptions) {
 	extraOptions.doSkip = true;
 	return Policies.find(databaseUtils.extendFilter({}, extraOptions), databaseUtils.extendOptions({}, extraOptions));
