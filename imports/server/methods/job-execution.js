a3031cf12c0478fe418a034df27c221fa7827dd7
Christoph Mussenbrock <christoph@etherisc.com>
2021-06-23 12:41:28 +0200

    meteor-kitchen 202106231041285241

--- imports/server/methods/job-execution.js
+++ imports/server/methods/job-execution.js
@@ -1,7 +1,7 @@
 import { readActivationsFile, countActivations, createPartners } from '/imports/server/methods/activations-reader.js';
 import { exportActivations } from '/imports/server/methods/activations-export.js';
 import { readLocationsFile, augmentLocations } from '/imports/server/methods/locations-reader.js';
-import { readGroupPoliciesFile, gp_aggregates, clear_selected } from '/imports/server/methods/group-policy-reader.js';
+import { readGroupPoliciesFile, gp_aggregates, clear_selected, gp_fix_id } from '/imports/server/methods/group-policy-reader.js';
 import { callApi } from '/imports/server/methods/call-api.js';
 import { runContractReview } from '/imports/server/methods/run-contract-review.js';
 
@@ -13,6 +13,7 @@ const jobs = {
 	readLocationsFile,
 	augmentLocations,
 	readGroupPoliciesFile,
+	gp_fix_id,
 	callApi,
 	runContractReview
 };
