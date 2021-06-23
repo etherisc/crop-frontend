b6e2b00664037ac65e6f6b1df512d5bcd16018da
Christoph Mussenbrock <christoph@etherisc.com>
2021-06-23 14:40:20 +0200

    meteor-kitchen 202106231240209570

--- client/views/group_policies/details/details.js
+++ client/views/group_policies/details/details.js
@@ -155,7 +155,7 @@ var GroupPoliciesDetailsGpIndividualPoliciesExport = function(fileType) {
 
 	var exportFields = [];
 
-	var gpId = this.params.groupPolicyId;
+	var gpId = "";
 
 	Meteor.call("gpIndividualPoliciesPagedExport", gpId, extraParams, exportFields, fileType, function(e, data) {
 		if(e) {
