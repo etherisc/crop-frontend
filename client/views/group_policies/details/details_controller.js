5fc597d6fb04016fbcc6042c3249537694c05acc
Christoph Mussenbrock <christoph@etherisc.com>
2021-06-23 14:46:55 +0200

    meteor-kitchen 202106231246549829

--- client/views/group_policies/details/details_controller.js
+++ client/views/group_policies/details/details_controller.js
@@ -58,16 +58,8 @@ this.GroupPoliciesDetailsController = RouteController.extend({
 		if(this.isReady() && this.gpIndividualPoliciesPagedExtraParams.pageNo >= data.gp_individual_policies_paged_page_count) {
 			Session.set("GpIndividualPoliciesPagedPageNo", data.gp_individual_policies_paged_page_count > 0 ? data.gp_individual_policies_paged_page_count - 1 : 0);
 		}
-data.gp_individual_policies_paged = Policies.find(
-	databaseUtils.extendFilter(
-		{group_policy_id:data.group_policy.id}, 
-		this.gpIndividualPoliciesPagedExtraParams
-	), 
-	databaseUtils.extendOptions(
-		{}, 
-		this.gpIndividualPoliciesPagedExtraParams
-	)
-);
+data.gp_individual_policies_paged = Policies.find({group_policy_id:data.group_policy.id});
+
 
 
 		return data;
