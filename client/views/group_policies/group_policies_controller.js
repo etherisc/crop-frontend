6c04fe08a2a2c6c990d37bee69acda988c41afa8
Christoph Mussenbrock <christoph@etherisc.com>
2021-06-25 15:56:23 +0200

    meteor-kitchen 202106251356237336

--- client/views/group_policies/group_policies_controller.js
+++ client/views/group_policies/group_policies_controller.js
@@ -18,7 +18,7 @@ this.GroupPoliciesController = RouteController.extend({
 	isReady: function() {
 		this.groupPolicyListPagedExtraParams = {
 			searchText: Session.get("GroupPolicyListPagedSearchString") || "",
-			searchFields: Session.get("GroupPolicyListPagedSearchFields") || ["id", "location.name", "sow_window", "sow_date", "begin_date", "end_date", "crop_stages", "payout", "meta"],
+			searchFields: Session.get("GroupPolicyListPagedSearchFields") || ["id", "location.name", "sow_window", "sow_date", "begin_date", "end_date", "payout.total_amount", "payout.deductible_amount", "payout.actual_amount"],
 			sortBy: Session.get("GroupPolicyListPagedSortBy") || "",
 			sortAscending: Session.get("GroupPolicyListPagedSortAscending"),
 			pageNo: Session.get("GroupPolicyListPagedPageNo") || 0,
