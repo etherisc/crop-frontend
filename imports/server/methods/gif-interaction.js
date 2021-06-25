28841aac12bec6de30e89cc6fae9bc3de55a4b42
Christoph Mussenbrock <christoph@etherisc.com>
2021-06-25 21:31:29 +0200

    meteor-kitchen 202106251931292421

--- imports/server/methods/gif-interaction.js
+++ imports/server/methods/gif-interaction.js
@@ -5,11 +5,19 @@ console.log('loading gif-interaction.js');
 const applyForPolicy = async (bpKey, data) => {
 
 	console.log(data);
-	return 'Success';
 
-	const result = await Product.applyForPolicy(bpKey, data);
+	try {
+		const result = await Product.applyForPolicy(bpKey, data);
 
-	info('Result of applyForPolicy:', result);
+		info('Result of applyForPolicy:', result);
+		return 'Success!';
+		
+	} catch (err) {
+		error('Error applyForPolicy', {message: err.message, stack: err.stack});
+		return err.message;
+	}
+	
+
 
 };
 
