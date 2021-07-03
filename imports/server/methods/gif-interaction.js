0c5185bb2623bb5ec6b23747d2d755bf89b515a8
Christoph Mussenbrock <christoph@etherisc.com>
2021-07-03 14:30:20 +0200

    meteor-kitchen 202107031230207474

--- imports/server/methods/gif-interaction.js
+++ imports/server/methods/gif-interaction.js
@@ -6,6 +6,7 @@ import { eth } from '/imports/server/methods/ethereum-provider.js';
 const applyForPolicy = async (args) => {
 
 	console.log(args);
+	console.log(eth);
 
 	const bpKey = eth.s32b('TestKey');
 	const data = eth.s32b('TestData');
