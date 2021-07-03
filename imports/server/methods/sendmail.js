0419292d584e6f59ab394d873704e61e13a2ca06
Christoph Mussenbrock <christoph@etherisc.com>
2021-07-03 14:24:15 +0200

    meteor-kitchen 202107031224151520

--- imports/server/methods/sendmail.js
+++ imports/server/methods/sendmail.js
@@ -4,34 +4,46 @@ import { settings } from '/imports/server/methods/settings.js';
 
 const nodemailer = require('nodemailer');
 
-const from = settings.('smtp.from');
-const host = settings.('smtp.host');
-const port = settings.('smtp.port');
-const user = settings.('smtp.user');
-const pass = settings.('smtp.pass');
+let transporter = null;
 
-const transport = {
-  host: "mail.acreafrica.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: "claims@acreafrica.com",
-    pass: ""
-  }
+const getTransporter = () => {
+
+	if (transporter) return transporter;
+
+	const from = settings.('smtp.from');
+	const host = settings.('smtp.host');
+	const port = settings.('smtp.port');
+	const user = settings.('smtp.user');
+	const pass = settings.('smtp.pass');
+
+	const transport = {
+		host,
+		port,
+		secure: true,
+		auth: {
+			user,
+			pass
+		}
+	};
+
+	try {
+		transporter = nodemailer.createTransport(transport);
+		return transporter;
+	} catch (err) {
+		error('Could not create SMTP transport', transport);
+		return null;
+	}
 };
 
 
-const transporter = nodemailer.createTransport(transport);
-
-
 const sendMail = async ({to, subject, text}) => {
-	
-	const mailInfo = await transporter.sendMail({from, to, subject, text});
-	info(`Mail sent to ${to}: ${subject}`, {from, to, subject, text, mailInfo});
-	
-	
+	if (const transporter = getTransporter()) {
+		const mailInfo = await transporter.sendMail({from, to, subject, text});
+		info(`Mail sent to ${to}: ${subject}`, {from, to, subject, text, mailInfo});
+	} else {
+		error('sendMail: No SMTP transport');
+	}
 };
 
 
 module.exports = { sendMail };
-	
