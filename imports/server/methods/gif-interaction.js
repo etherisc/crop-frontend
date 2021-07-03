c70d250043ef18254bedf088b66c720949c84845
Christoph Mussenbrock <christoph@etherisc.com>
2021-07-03 17:51:39 +0200

    meteor-kitchen 202107031551396269

--- imports/server/methods/gif-interaction.js
+++ imports/server/methods/gif-interaction.js
@@ -2,20 +2,23 @@ console.log('loading gif-interaction.js');
 
 import { eth } from '/imports/server/methods/ethereum-provider.js';
 import { settings } from '/imports/server/methods/settings.js';
+import { v4 as uuidv4 } from 'uuid';
 
-const applyForPolicy = async (args) => {
-
-	const bpKey = eth.s32b('TestKey');
-	const data = eth.s32b('TestData');
-
-	try {
-		const Product = new eth.ethers.Contract(
+const Product = new eth.ethers.Contract(
 			settings('gif.product.address'), 						
 			settings('gif.product.abi'), 
 			eth.wallet()
-		);		
+		);	
 
-		const result = await Product.applyForPolicy(bpKey, data, {from: settings('gif.from'), gasLimit: 500000});
+
+const applyForPolicy = async (args) => {
+
+	const bpKey = eth.s32b(uuidv4());
+	const data = eth.s32b('TestData');
+
+	try {
+
+		const result = await Product.applyForPolicy(bpKey, data, {gasLimit: 500000});
 
 		info('Result of applyForPolicy:', result);
 		return 'Success!';
