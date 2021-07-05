54b708531adcf85070ad897bc40503d3b855e3e7
Christoph Mussenbrock <christoph@etherisc.com>
2021-07-05 11:39:02 +0200

    meteor-kitchen 202107050939028821

--- imports/server/methods/gif-interaction.js
+++ imports/server/methods/gif-interaction.js
@@ -74,7 +74,7 @@ const applyForPolicy = async (args) => {
 	console.log(bpKey, text, hash);
 	const {receipt: {transactionHash, blockNumber}} = await contractCall('applyForPolicy', bpKey, hash);
 	
-	const apply = {text, hash, transactionHash, blockNumber, timestamp: eth.blockTimestamp(blockNumber)};
+	const apply = {text, hash, transactionHash, blockNumber, timestamp: await eth.blockTimestamp(blockNumber)};
 	
 	Policies.update({_id}, {$set: {bc_trail: {apply}, next_action: 'underwrite'}});
 	info(`applyForPolicy ${bpKey}`, {apply});
