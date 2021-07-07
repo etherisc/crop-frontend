971bd0e5eb7f304c84fa86d5c9a9e85fafd3a53a
Christoph Mussenbrock <christoph@etherisc.com>
2021-07-07 17:33:12 +0200

    meteor-kitchen 202107071533128157

--- imports/server/methods/payout-schedule.js
+++ imports/server/methods/payout-schedule.js
@@ -3,6 +3,7 @@ console.log('loading payout-schedule.js');
 import { sendMail } from '/imports/server/methods/sendmail.js';
 import { bongaSMS } from '/imports/server/methods/bonga-gateway.js';
 import { settings } from '/imports/server/methods/settings.js';
+import { sendTelegram } from '/imports/server/methods/telegram-transport.js';
 
 const clearPayoutSchedule = (_id) => {
 
