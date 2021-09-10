52f4f4b451b801685d53e3aa05c68c05b802194a
Christoph Mussenbrock <christoph@etherisc.com>
2021-09-10 11:05:07 +0200

    meteor-kitchen 202109100905072609

--- imports/server/methods/policy-notarization.js
+++ imports/server/methods/policy-notarization.js
@@ -7,43 +7,60 @@ import { applyForPolicy, underwrite, claim, payout } from '/imports/server/metho
 const noop = () => null;
 
 const notarizePolicy = async (policy) => {
-	
+
 	info('NotarizePolicy', policy);
-	
-	try { await applyForPolicy({ policy }); } catch (err) { noop(); }
-	try { await underwrite({ policy }); } catch (err) { noop(); }
-	
+
+	try { 
+		await applyForPolicy({ policy }); 
+		sendTelegram(`applyForPolicy ${policy._id}`);
+	} catch (err) { 
+		noop(); 
+	}
+
+	try { 
+		await underwrite({ policy }); 
+		sendTelegram(`underwrite ${policy._id}`);
+	} catch (err) { 
+		noop(); 
+	}
+
 	if (policy.claims) {
-		
+
 		for (let claimIndex = 0; claimIndex < policy.claims.length; claimIndex += 1) {
 			const thisClaim = policy.claims[claimIndex]
 			if (
 				thisClaim.status == "Confirmed" && 
 				thisClaim.amount > 0
 			) {
-				try { await claim({ policy }, claimIndex); } catch (err) { noop(); }
+				try { 
+					await claim({ policy }, claimIndex); 
+					sendTelegram(`claim ${policy._id} #${claimIndex}`);
+
+				} catch (err) { 
+					noop(); 
+				}
 			}
 		}
-		
+
 	} else {
 		info(`No claims for Policy ${policy._id}`);
 	}
-	
+
 }
 
 
 const pauseFor = async (milliSec) => await new Promise((resolve) => setTimeout(resolve, milliSec));
-	
+
 
 const notarizeManyPolicies = async ({ filter, maxPolicies }) => {
 
 	const policies = Policies.find().fetch();
-		
+
 	for (let pIndex = 0; pIndex < maxPolicies; pIndex += 1) {
-		
+
 		notarizePolicy(policies[pIndex]).catch(err => noop());
 		await pauseFor(1000);
-		
+
 	}
 
 }
